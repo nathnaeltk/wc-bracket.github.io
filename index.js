2546f2e5c446cfeb3999a5eb12a2417fdf8a1492
Nathnael Teketel <hellonathnael@gmail.com>
2022-12-03 11:19:05 +0300

    initial js

---
+++ index.js
@@ -0,0 +1,126 @@
+// Round of sixteen
+$(".rosone").click(function () {
+    $(".roeone").text($(".rosone").text());
+});
+
+$(".rostwo").click(function () {
+    $(".roeone").text($(".rostwo").text());
+});
+
+$(".rosthree").click(function () {
+    $(".roetwo").text($(".rosthree").text());
+});
+
+$(".rosfour").click(function () {
+    $(".roetwo").text($(".rosfour").text());
+});
+
+$(".rosfive").click(function () {
+    $(".roethree").text($(".rosfive").text());
+});
+
+$(".rossix").click(function () {
+    $(".roethree").text($(".rossix").text());
+});
+
+$(".rosseven").click(function () {
+    $(".roefour").text($(".rosseven").text());
+});
+
+$(".roseight").click(function () {
+    $(".roefour").text($(".roseight").text());
+});
+
+$(".rosnine").click(function () {
+    $(".roefive").text($(".rosnine").text());
+});
+
+$(".rosten").click(function () {
+    $(".roefive").text($(".rosten").text());
+});
+
+$(".roseleven").click(function () {
+    $(".roesix").text($(".roseleven").text());
+});
+
+$(".rostwelve").click(function () {
+    $(".roesix").text($(".rostwelve").text());
+});
+
+$(".rostherteen").click(function () {
+    $(".roeseven").text($(".rostherteen").text());
+});
+
+$(".rosfourteen").click(function () {
+    $(".roeseven").text($(".rosfourteen").text());
+});
+
+$(".rosfifteen").click(function () {
+    $(".roeeight").text($(".rosfifteen").text());
+});
+
+$(".rossixteen").click(function () {
+    $(".roeeight").text($(".rossixteen").text());
+});
+
+
+// round of eight
+$(".roeone").click(function () {
+    $(".rofone").text($(".roeone").text());
+});
+
+$(".roetwo").click(function () {
+    $(".rofone").text($(".roetwo").text());
+});
+
+$(".roethree").click(function () {
+    $(".roftwo").text($(".roethree").text());
+});
+
+$(".roefour").click(function () {
+    $(".roftwo").text($(".roefour").text());
+});
+
+$(".roefive").click(function () {
+    $(".rofthree").text($(".roefive").text());
+});
+
+$(".roesix").click(function () {
+    $(".rofthree").text($(".roesix").text());
+});
+
+$(".roeseven").click(function () {
+    $(".roffour").text($(".roeseven").text());
+});
+
+$(".roeeight").click(function () {
+    $(".roffour").text($(".roeeight").text());
+});
+
+//round of four
+
+$(".rofone").click(function () {
+    $(".finalleft").text($(".rofone").text());
+});
+
+$(".roftwo").click(function () {
+    $(".finalleft").text($(".roftwo").text());
+});
+
+$(".rofthree").click(function () {
+    $(".finalright").text($(".rofthree").text());
+});
+
+$(".roffour").click(function () {
+    $(".finalright").text($(".roffour").text());
+});
+
+//final
+
+$(".finalleft").click(function () {
+    $(".champ").text($(".finalleft").text());
+});
+
+$(".finalright").click(function () {
+    $(".champ").text($(".finalright").text());
+});
